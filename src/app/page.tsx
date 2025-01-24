0c66526f1ad4602ea0a954290fd300c7c20b951c
nick.peng <nick.peng@cloud-interactive.com>
2025-01-24 10:50:58 +0800

    merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -219,7 +219,7 @@ export default function Home() {
                     src="/image/gamer.png"
                     alt="gamer"
                     className={`m-auto transition duration-[15s] ${
-                      currentIndex > 1 ? 'brightness-[0.3]' : 'brightness-[0]'
+                      currentIndex > 5 ? 'brightness-[0.5]' : 'brightness-[0]'
                     }`}
                   />
                 </div>
