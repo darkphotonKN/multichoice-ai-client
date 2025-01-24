e26f69d27d837087a41aefecbfe85427c57f27d7
kiki <amyvsvivi@gmail.com>
2025-01-24 12:16:53 +0800

    Solve the issue of continuously calling APIs during a countdown.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ const text = "I WANT TO PLAY A GAME...";
 const answerSeconds = 10;
 
 const domain = "https://bd35-210-242-7-79.ngrok-free.app";
-
 interface Questions {
   [key: number]: { question: string; answers: { id: string; text: string }[] };
 }
@@ -27,75 +26,59 @@ const questions: Questions = {
   2: {
     question: "你媽?",
     answers: [
-      { id: "A", text: "Berlin" },
-      { id: "B", text: "Madrid" },
-      { id: "C", text: "Paris" },
-      { id: "D", text: "Rome" },
+      { id: "A", text: "Google" },
+      { id: "B", text: "IBM" },
+      { id: "C", text: "Gray" },
+      { id: "D", text: "OpenAI" },
+    ],
+  },
+  3: {
+    question:
+      "任天堂(Nintendo)的耀西(Yoshi)在2019年後被官方證實是哪一種動物(animals)?",
+    answers: [
+      { id: "A", text: "蜥蜴(Lizard)" },
+      { id: "B", text: "恐龍(Dinosaur)" },
+      { id: "C", text: "小卷(Squid)" },
+      { id: "D", text: "烏龜(Turtle)" },
+    ],
+  },
+  4: {
+    question: "在吉伊卡哇(Chīkawa)動畫中主角(ちいかわ)代表哪一種動物(animals)?",
+    answers: [
+      { id: "A", text: "北極熊(Polar bear)" },
+      { id: "B", text: "哈姆太郎(Hamtaro)" },
+      { id: "C", text: "飛鼠(Flying squirrel)" },
+      { id: "D", text: "兔子(Rabbit)" },
+    ],
+  },
+  5: {
+    question:
+      "In the Netflix Korean drama Squid Game, what is the first game the contestants play?",
+    answers: [
+      { id: "A", text: "One, two, three, freeze!" },
+      { id: "B", text: "Tug of War" },
+      { id: "C", text: "Marbles" },
+      { id: "D", text: "Ride bike" },
     ],
   },
 };
 
 export default function Home() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [step, setStep] = useState(1);
   const [questionNumber, setQuestionNumber] = useState(0);
   const [aiThinking, setAiThinking] = useState(false);
+  // 開啟和關掉 ai 思考狀態的文字
   const [isAIAnswer, setIsAIAnswer] = useState(false);
   const [gamerSelectedAnswer, setGamerSelectedAnswer] = useState<string | null>(
-    null,
+    null
   );
   const [AISelectedAnswer, setAISelectedAnswer] = useState<string | null>(null);
 
+  // 倒數計時會不斷重新觸發 Ai
+  const [aiHasAnswered, setAiHasAnswered] = useState<boolean>(false);
+
   const [displayText, setDisplayText] = useState("");
   const [currentIndex, setCurrentIndex] = useState(0);
-  // const question = "What is the capital of France?";
-  // const question = "What is the highest mountain peak in the solar system?";
-  // const question =
-  // "In the Netflix Korean drama Squid Game, what is the first game the contestants play?";
-  const question =
-    "任天堂(Nintendo)的耀西(Yoshi)在2019年後被官方證實是哪一種動物(animals)?";
-  // const question =
-  //   "在吉伊卡哇(Chīkawa)動畫中主角(ちいかわ)代表哪一種動物(animals)?";
-
-  // const answers = [
-  //   { id: "A", text: "Berlin" },
-  //   { id: "B", text: "Madrid" },
-  //   { id: "C", text: "Paris" },
-  //   { id: "D", text: "Rome" },
-  // ];
-  // const answers = [
-  //   { id: "A", text: "Mount Everest" },
-  //   { id: "B", text: "Mount Kilimanjaro" },
-  //   { id: "C", text: "Olympus Mons" },
-  //   { id: "D", text: "Mount Fuji Response" },
-  // ];
-  // const answers = [
-  //   { id: "A", text: "One, two, three, freeze!" },
-  //   { id: "B", text: "Tug of War" },
-  //   { id: "C", text: "Marbles" },
-  //   { id: "D", text: "Ride bike" },
-  // ];
-  const answers = [
-    { id: "A", text: "蜥蜴(Lizard)" },
-    { id: "B", text: "恐龍(Dinosaur)" },
-    { id: "C", text: "小卷(Squid)" },
-    { id: "D", text: "烏龜(Turtle)" },
-  ];
-  // const answers = [
-  //   { id: "A", text: "北極熊(Polar bear)" },
-  //   { id: "B", text: "哈姆太郎(Hamtaro)" },
-  //   { id: "C", text: "飛鼠(Flying squirrel)" },
-  //   { id: "D", text: "兔子(Rabbit)" },
-  // ];
-
-  // submit answer for the round
-  // const handleSubmit = () => {
-  //   if (!AISelectedAnswer) {
-  //     alert('pleaseeeeee select an answer before submitting.');
-  //     return;
-  //   }
-  //   console.log('Selected Answer:', AISelectedAnswer);
-  // };
 
   // test AI choosing answer
   async function handleAIChooseAnswer() {
@@ -123,8 +106,10 @@ export default function Home() {
         •	Respond with a single letter (A, B, C, or D) corresponding to the correct answer.
 
         Now answer the following question and match your answer to the correct option
-      Question: ${question}
-      Options: ${answers.map(({ id, text }) => `${id}. ${text}`)}
+      Question: ${questions[questionNumber].question}
+      Options: ${questions[questionNumber].answers.map(
+        ({ id, text }) => `${id}. ${text}`
+      )}
       Tell me the correct option—respond with the shortest answer possible, nothing else.
     `,
     });
@@ -157,17 +142,14 @@ export default function Home() {
   };
 
   useEffect(() => {
-    const interval = setInterval(
-      () => {
-        if (currentIndex < text.length) {
-          setDisplayText((prevText) => prevText + text[currentIndex]);
-          setCurrentIndex((prevIndex) => prevIndex + 1);
-        } else {
-          clearInterval(interval);
-        }
-      },
-      typingSpeed + (currentIndex === 0 ? 6000 : 0),
-    );
+    const interval = setInterval(() => {
+      if (currentIndex < text.length) {
+        setDisplayText((prevText) => prevText + text[currentIndex]);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      } else {
+        clearInterval(interval);
+      }
+    }, typingSpeed + (currentIndex === 0 ? 6000 : 0));
 
     return () => {
       clearInterval(interval);
@@ -193,43 +175,44 @@ export default function Home() {
     setSeconds(answerSeconds);
     setQuestionNumber((prev) => prev + 1);
     setIsAIAnswer(false);
+    setAiHasAnswered(false); // 一題只能回答一次 允許下一題作答
   };
 
   useEffect(() => {
-    if (seconds <= 8 && !isAIAnswer) {
+    if (seconds <= 8 && !aiHasAnswered) {
       handleAIChooseAnswer();
+      setAiHasAnswered(true); // 確保每題AI只回答一次
     }
     if (seconds === 0) {
       apiEnd();
     }
   }, [seconds, isAIAnswer]);
 
-  console.log("questions[questionNumber]", questions[questionNumber]);
-  console.log("AISelectedAnswer", AISelectedAnswer);
   function renderStep() {
     switch (step) {
       case 1: {
         return (
           <div
-            className="text-center font-bold cursor-pointer"
+            className='text-center font-bold cursor-pointer'
             onClick={() => {
               handleNext();
               setStep(2);
             }}
           >
-            <div className="flex text-center justify-center">
+            <div className='flex text-center justify-center'>
               <div>
-                <div className="text-center">
+                <div className='text-center'>
                   <img
-                    src="/image/gamer.png"
-                    alt="gamer"
-                    className={`m-auto transition duration-[15s] ${currentIndex > 5 ? "brightness-[0.5]" : "brightness-[0]"
-                      }`}
+                    src='/image/gamer.png'
+                    alt='gamer'
+                    className={`m-auto transition duration-[15s] ${
+                      currentIndex > 5 ? "brightness-[0.5]" : "brightness-[0]"
+                    }`}
                   />
                 </div>
-                <div className="h-[120px] text-[120px] text-[#cc0000] font-[ghastly-panic] flex">
+                <div className='h-[120px] text-[120px] text-[#cc0000] font-[ghastly-panic] flex'>
                   {displayText}
-                  <div className="cursor ml-4"></div>
+                  <div className='cursor ml-4'></div>
                 </div>
               </div>
             </div>
@@ -239,31 +222,31 @@ export default function Home() {
       case 2: {
         return (
           <>
-            <h2 className="text-[80px] font-[new-tegomin-regular] text-[#aa0000]">
+            <h2 className='text-[80px] font-[new-tegomin-regular] text-[#aa0000]'>
               問題{questionNumber}
             </h2>
-            <div className="w-[1000px] mx-auto mt-10 shadow-lg rounded-lg">
+            <div className='w-[1000px] mx-auto mt-10 shadow-lg rounded-lg'>
               {/* <h1 className="text-2xl font-bold mb-4"></h1> */}
-              <p className="text-[80px] text-[#aa0000] mb-6 font-[new-tegomin-regular]">
-                {questions[questionNumber].question}
+              <p className='text-[80px] text-[#aa0000] mb-6 font-[new-tegomin-regular]'>
+                {questions[questionNumber]?.question}
               </p>
 
-              <ul className="space-y-4">
-                {questions[questionNumber].answers.map((answer) => (
+              <ul className='space-y-4'>
+                {questions[questionNumber]?.answers.map((answer) => (
                   <li
                     key={answer.id}
-                    className="text-xl text-[#aa0000] text-[40px] pt-[30px] cursor-pointer"
+                    className='text-xl text-[#aa0000] text-[40px] pt-[30px] cursor-pointer'
                     onClick={() => setGamerSelectedAnswer(answer.id)}
                   >
-                    <div className="flex gap-2 font-[new-tegomin-regular] ">
-                      <span className="font-medium">{answer.id})</span>{" "}
+                    <div className='flex gap-2 font-[new-tegomin-regular] '>
+                      <span className='font-medium'>{answer.id})</span>{" "}
                       {answer.text}
                       {gamerSelectedAnswer === answer.id && (
                         <Image
                           width={24}
                           height={24}
                           src={"/image/ai.png"}
-                          alt="AI IS HERE"
+                          alt='AI IS HERE'
                         />
                       )}
                       {AISelectedAnswer === answer.id && (
@@ -271,7 +254,7 @@ export default function Home() {
                           width={24}
                           height={24}
                           src={"/image/ai.png"}
-                          alt="AI IS HERE"
+                          alt='AI IS HERE'
                         />
                       )}
                     </div>
@@ -281,17 +264,17 @@ export default function Home() {
 
               {/* AI Thinking Corner */}
               {aiThinking ? (
-                <div className="mt-[20px] flex gap-2 pt-2 w-100 text-center font-medium text-[#aa0000] font-[new-tegomin-regular]">
+                <div className='mt-[20px] flex gap-2 pt-2 w-100 text-center font-medium text-[#aa0000] font-[new-tegomin-regular]'>
                   <Image
                     width={24}
                     height={24}
                     src={"/image/ai.png"}
-                    alt="AI IS HERE"
+                    alt='AI IS HERE'
                   />{" "}
                   : 讓我思考一下...
                 </div>
               ) : (
-                <div className="mt-4"></div>
+                <div className='mt-4'></div>
               )}
 
               {/* <button
@@ -301,11 +284,11 @@ export default function Home() {
                 AI Answer
               </button> */}
 
-              <div className="w-full text-right">
+              <div className='w-full text-right'>
                 {seconds === 0 && (
                   <button
                     onClick={handleNext}
-                    className="mt-6  text-[#aa0000] font-[new-tegomin-regular] font-bold py-2 px-4 text-[40px]"
+                    className='mt-6  text-[#aa0000] font-[new-tegomin-regular] font-bold py-2 px-4 text-[40px]'
                   >
                     下一題
                   </button>
@@ -319,9 +302,9 @@ export default function Home() {
   }
 
   return (
-    <div className="p-8">
+    <div className='p-8'>
       {step >= 2 && (
-        <div className="text-[120px] text-center text-[#aa0000] font-[new-tegomin-regular]">
+        <div className='text-[120px] text-center text-[#aa0000] font-[new-tegomin-regular]'>
           {seconds}
         </div>
       )}
